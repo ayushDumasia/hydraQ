a987031bf2468554b2373681bf06c8e3be77d6b6
Harsh <harshpatel120605@gmail.com>
2024-12-08 16:45:07 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request, Response
+
+from chatbot.chat_utils import fetchChatHistory, chatWithChain
+
+app = Flask(__name__)
+
+@app.route("/chat/<session_id>", methods = ['GET', 'POST'])
+def disp(session_id):
+    input = request.get_json(force=True)
+    user_id = input.get('user_id')
+    user_input = input.get('user_input')
+
+    if not input:
+        return jsonify({'error': 'No user input provided'}), 400
+
+    chat_history = fetchChatHistory(user_id, session_id)
+    return Response(chatWithChain(user_input, chat_history), content_type='text/plain')
+
+if __name__ == '__main__':
+    app.run(debug = True)
